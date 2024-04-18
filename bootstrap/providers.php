34f7ed886a4caeba849114a8ab9363868f0caf4f
padrada49 <tuktuk49@gmail.com>
2024-04-18 11:06:18 +0700

    Update Repository

---
+++ bootstrap/providers.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    App\Providers\AppProvidersRepositoriesProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\FortifyServiceProvider::class,
+    App\Providers\JetstreamServiceProvider::class,
+];
